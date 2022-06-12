f8274cf451bdfb7145049f7092111569b5203f07
6ssou <6ssou@protonmail.com>
2022-06-12 14:14:20 +0200

    add event MultiSigCreated

--- ethereum/contracts/MultiSigFactory.sol
+++ ethereum/contracts/MultiSigFactory.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: 6ssou
 import "./MultiSig.sol";
+// 0xD47Bc32A186Db5Bb09E3312CA32DCf27F1ce3b6A - 12/06/2022
 pragma solidity ^0.8.0;
 
 contract MultiSigFactory {
+
+    event MultiSigCreated(address[] owners, address creator);
 
     uint multiSigId;
     // mapping of creator by multisig wallet
@@ -16,6 +19,7 @@ contract MultiSigFactory {
     function createMultiSigWallet(address[] memory _owners, uint _numConfirmationRequired) public {
         multiSigs[multiSigId] = new MultiSig(_owners, _numConfirmationRequired);
         creator[multiSigId] = address(msg.sender);
+        emit MultiSigCreated(_owners, creator[multiSigId]);
         multiSigId++;
     }
     function getDeployedMultiSigCount() public view returns(uint) {
